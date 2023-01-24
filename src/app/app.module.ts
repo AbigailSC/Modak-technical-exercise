d7337ab12262db4f38bf4578ba213ed669b263a5
AbigailSc <abigailsarzuri@gmail.com>
2023-01-24 19:59:15 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BreadscrumbComponent } from './breadscrumb/breadscrumb.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadscrumbComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
